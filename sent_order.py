1a1c1100112c610065fcdb1d4482ef816f7b066d
danlinhqn <opinvn@gmail.com>
2024-03-15 14:24:36 +0700

    update

--- sent_order.py
+++ sent_order.py
@@ -3,20 +3,21 @@ from kafka import KafkaProducer
 import json
 from notify_client import *
 
-# Hàm này sẽ gửi thông tin đặt hàng từ Client lên Kafka
+# Use for sent order from client to Kafka
 def sent_Order_From_Client():
-    # Danh sách các topic tùy chọn
+
+    # List topics for user choose
     valid_topics = ["Clothing", "Cosmetics"]
 
-    # Hiển thị danh sách tùy chọn cho người dùng
+    # Show list topic for user choose
     print("Please choose product type:")
     for i, topic in enumerate(valid_topics, 1):
         print(f"{i}. {topic}")
 
-    # Yêu cầu người dùng chọn một topic
+    # Choice topic
     choice = input("Select a topic by entering the corresponding number: ").strip()
 
-    # Kiểm tra xem lựa chọn có hợp lệ không
+    # Check after user choice
     try:
         choice_index = int(choice)
         if choice_index < 1 or choice_index > len(valid_topics):
@@ -25,38 +26,39 @@ def sent_Order_From_Client():
         print("Wrong choice!")
         exit()
 
-    # Chọn topic dựa trên lựa chọn của người dùng
+    # Choice topic base on user choice
     topic_name = valid_topics[choice_index - 1]
 
-    # Khai báo KafkaProducer với topic tương ứng
+    # Declare KafkaProducer with the corresponding topic
     producer = KafkaProducer(
         bootstrap_servers=config.kafka_ip
     )
 
-    # Nhập dữ liệu từ người dùng
+    # Input data from user
     t_shirt = input("Product's Name: ")
     client_name = input("Your Name: ")
     age = input("Qty Want Buy: ")
 
-    # Tạo đối tượng JSON từ dữ liệu người dùng
+    # Create JSON object from user data
     data = {"Product's Name:": t_shirt, "Client's Name": client_name, "Qty": int(age)}
     json_data = json.dumps(data)
 
-    # Gửi dữ liệu đến topic
+    # Send data to topic
     producer.send(topic_name, json_data.encode('utf-8'))
-    # Flush dữ liệu
+
+    # Flush data
     producer.flush()
 
-    # Thông báo ngay khi đặt hàng xong bước 1
+    # Nofity to user after sent order step 1
     print("Order sent!, please wait for the confirmation from the Shop ...")
 
-    # Tại đây sẽ chạy file nhận thông báo từ Shop
+    # step 2, in here we will receive notify from shop to client
     receive_Notify_From_Shop_To_Client()
     
     print("$ ---------------------------------- $")
     print("Buy more, happy more :)")
     
-# Hàm chạy chương trình
+# Use for run program
 def run_program():
     while True:
         try:
@@ -66,8 +68,8 @@ def run_program():
             print("Retry in 15 seconds...")
             time.sleep(5)
             continue
-        time.sleep(1)  # Chờ 10 giấy trước khi chạy lại
+        time.sleep(1)  # Wait for 1 second
         
-# Chạy chương trình
+# Run program
 print("Begin order product in Shop ...")
 run_program()
